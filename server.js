46e61e40fdf85203262237dea8340a8adda38468
Bussu <vbussu@lbps.com>
2018-07-10 12:00:56 -0700

    for heroku changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+var port=process.env.PORT ||3000;
 var app = express();
 
 hbs.registerPartials(__dirname +'/views/partials');
@@ -59,6 +61,6 @@ app.get('/bad',(req,res)=>{
    });
 
 
-app.listen(3000,()=>{
-    console.log("Server is up ")
+app.listen(port,()=>{
+    console.log(`Server is up on ${port} `)
 });
